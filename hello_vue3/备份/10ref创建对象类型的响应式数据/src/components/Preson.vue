ff76d87c8d7ed349c71078b69f6e1cfab212598d
Searching-Sprinkle <l13012032112@gmail.com>
2024-08-23 10:00:43 +0800

    8.23

---
+++ hello_vue3/备份/10ref创建对象类型的响应式数据/src/components/Preson.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="person">
+        <h2>一辆{{ car.color }}色{{ car.brand }}车，价值{{ car.price }}万</h2>
+        <button @click="changePrice">修改汽车的价格</button>
+        <button @click="reSet">重置</button>
+        <br>
+        <h2>游戏列表</h2>
+        <ul>
+            <li v-for="item in games" :key="item.id">
+<!-- :的含义就是绑定属性，:key="item.id"的意思是把item.id的值绑定到key属性上 -->
+ {{ item.name }}
+            </li>
+        </ul>
+        <button @click="changeGmae1Name">修改第一个游戏的名字</button>
+    </div>    
+</template>
+<!-- <script  lang="ts">
+ //JS或Ts代码
+ //这里是一个组件，自定义组件名
+ export default {
+    name: 'Preson',
+}
+</script> -->
+
+<script setup lang="ts" name="Presonal">
+import { reactive,ref } from 'vue'
+    //数据
+    let car =ref( {
+        brand:'奔驰',
+        price:100,
+        color:'黑'
+        })
+    let games =ref([
+        {id:'game1',name:'黑神话'},
+        {id:'game2',name:'英雄联盟'},
+        {id:'game3',name:'王者荣耀'}
+    ])
+    function changePrice() {
+        car.value.price += 20;
+    }
+    function changeGmae1Name() {
+        games.value[0].name = '黑神话·悟空';
+    }
+    function reSet() {
+        car.value.price = 100;
+        games.value[0].name = '黑神话';
+    }
+</script>
+
+<style scoped>
+    /* css样式 */
+    .person {
+        background-color: rgb(255, 221, 127);
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 10px;
+    }
+</style>
