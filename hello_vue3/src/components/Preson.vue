63a358497274b44518a4e76b4b59cc0728e535ba
Searching-Sprinkle <l13012032112@gmail.com>
2024-08-25 10:49:17 +0800

    8.25

--- hello_vue3/src/components/Preson.vue
+++ hello_vue3/src/components/Preson.vue
@@ -1,24 +1,21 @@
 <template>
     <div class="person">
-        <h2>一辆{{ car.color }}色{{ car.brand }}车，价值{{ car.price }}万</h2>
-        <button @click="changePrice">修改汽车的价格</button>
-        <button @click="changeCar">变个reactive汽车</button>
-        <button @click="reSet">重置</button>
-        <br>
-        <h2>游戏列表</h2>
-        <ul>
-            <li v-for="item in games" :key="item.id">
-<!-- :的含义就是绑定属性，:key="item.id"的意思是把item.id的值绑定到key属性上 -->
- {{ item.name }}
-            </li>
-        </ul>
-        <button @click="changeGmae1Name">修改第一个游戏的名字</button>
-        <hr>
-        <h2>一辆{{ car1.color }}色{{ car1.brand }}车，价值{{ car1.price }}万</h2>
-        <button @click="changeCar1">变个ref汽车</button>
+      <h2>人名：{{ preson.name }}</h2>
+      <h2>年龄：{{ preson.age }}</h2>
+      <h2>性别：{{ preson.sex }}</h2>
+      <button @click="changeAge">年龄+1</button>
+      <button @click="changeName">改名字</button>
+      <button @click="Reset">重置信息</button>
+      <hr>
+      <h2>人名：{{ name }}</h2>
+      <h2>年龄：{{ age }}</h2>
+      <h2>性别：{{ sex }}</h2>
+      <button @click="changetoRefsAge">年龄+1</button>
+      <button @click="changetoRefsName">改名字</button>
+      <button @click="ResettoRefs">重置信息</button>
     </div> 
-   
 </template>
+
 <!-- <script  lang="ts">
  //JS或Ts代码
  //这里是一个组件，自定义组件名
@@ -28,40 +25,38 @@
 </script> -->
 
 <script setup lang="ts" name="Presonal">
-import { reactive,ref } from 'vue'
-    //数据
-    let car1 =ref({
-        brand:'奔驰',
-        price:100,
-        color:'黑'
-        })
+import { reactive,toRefs } from 'vue'
+let preson = reactive({
+    name: '张三',
+    age: 18,
+    sex: '男'
+})
 
-    let car =reactive( { //reactive再次分配对象时，失去响应式的属性，用ref就没有这种烦恼
-        brand:'奔驰',
-        price:100,
-        color:'黑'
-        })
-    let games =ref([
-        {id:'game1',name:'黑神话'},
-        {id:'game2',name:'英雄联盟'},
-        {id:'game3',name:'王者荣耀'}
-    ])
-    function changePrice() {
-        car.price += 20;
-    }
-    function changeGmae1Name() {
-        games.value[0].name = '黑神话·悟空';
-    }
-    function changeCar() {
-       Object.assign(car,{brand:'宝马',price:200,color:'白'})//reactive这么写能改
-    }
-    function changeCar1() {
-        car1.value = {brand:'宝马',price:200,color:'白'}
-    }
-    function reSet() {
-        car.price = 100;
-        games.value[0].name = '黑神话';
-    }
+//let {name,age,sex}=preson//解构赋值，但不是响应式的
+let {name,age,sex}=toRefs(preson)//解构赋值，是响应式的
+function changeAge() {
+    preson.age++
+}
+function changeName() {
+    preson.name = '李四'
+}
+function changetoRefsAge() {
+    age.value++
+}
+function changetoRefsName() {
+    name.value = '王五'
+}
+function ResettoRefs() {
+    name.value = '张三'
+    age.value = 18
+    sex.value = '男'
+}
+function Reset() {
+    preson.name = '张三'
+    preson.age = 18
+    preson.sex = '男'
+}
+  
 </script>
 
 <style scoped>
