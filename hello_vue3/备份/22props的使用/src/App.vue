0d6211a4bfe7bac8ad094a0e71db1235cb26d38d
Searching-Sprinkle <l13012032112@gmail.com>
2024-09-01 09:53:50 +0800

    9.1

---
+++ hello_vue3/备份/22props的使用/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- HTML -->
+    <Person a="你好" />
+</template>
+
+<script lang="ts" setup name = "App">
+ //JS或Ts代码
+ import Person from '@/components/Person.vue'
+import { ref, reactive} from 'vue'
+import { type PersonInter,type Persons } from '@/types';
+
+let person:PersonInter = {
+    age:18,
+    id:'123',
+    name:'张三',
+    sex:'男'
+}
+
+let personList:Persons = reactive<Persons>([
+    {
+        age:18,
+        id:'123',
+        name:'张三',
+        sex:'男'
+    },
+    {
+        age:19,
+        id:'123',
+        name:'李四',
+        sex:'女'
+    },
+    {
+        age:20,
+        id:'123',
+        name:'王五',
+        sex:'男'
+    }
+])
+</script>
---
+++ hello_vue3/备份/22props的使用/src/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+<ul>
+    <li v-for="(item,index) in list" :key="index">{{item.name}}--{{ item.age }}</li>
+</ul>
+    </div> 
+</template>
+
+<script setup lang="ts" name="Person" >
+import { defineProps,withDefaults } from 'vue';
+import { type PersonInter,type Persons } from '@/types';
+
+//接受list并限制类型
+// defineProps<{list:Persons}>()
+
+//接受list+限制类型+限制必要性+指定默认值
+withDefaults(defineProps<{list?:Persons,a:string}>(),{
+    list:()=>[
+        {
+            age:21,
+            id:'45456456',
+            name:'康师傅',
+            sex:'武装直升机'
+        }
+    ],
+    a:'你好'
+})
+
+
+
+//只接收list
+// defineProps(['a','list'])
+
+//接受list，同时将props保存起来
+// let x = defineProps(['a','list'])
+</script>
+
+<style scoped>
+    /* css样式 */
+    .person {
+        background-color: rgb(255, 221, 127);
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 10px;
+    }
+</style>
