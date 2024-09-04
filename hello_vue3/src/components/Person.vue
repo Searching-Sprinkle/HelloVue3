77adc825fbdbba2b1dbe429c49d3e3e6c7227d70
Searching-Sprinkle <l13012032112@gmail.com>
2024-09-04 10:05:37 +0800

    9.4

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -39,41 +39,6 @@ onBeforeUnmount(()=>{
 onUnmounted(()=>{
     console.log('卸载完毕')
 })
-=======
-<ul>
-    <li v-for="(item,index) in list" :key="index">{{item.name}}--{{ item.age }}</li>
-</ul>
-    </div> 
-</template>
-
-<script setup lang="ts" name="Person" >
-import { defineProps,withDefaults } from 'vue';
-import { type PersonInter,type Persons } from '@/types';
-
-//接受list并限制类型
-// defineProps<{list:Persons}>()
-
-//接受list+限制类型+限制必要性+指定默认值
-withDefaults(defineProps<{list?:Persons,a:string}>(),{
-    list:()=>[
-        {
-            age:21,
-            id:'45456456',
-            name:'康师傅',
-            sex:'武装直升机'
-        }
-    ],
-    a:'你好'
-})
-
-
-
-//只接收list
-// defineProps(['a','list'])
-
-//接受list，同时将props保存起来
-// let x = defineProps(['a','list'])
->>>>>>> 394f90a678656f1ed413341391734435bdcf4268
 </script>
 
 <style scoped>
