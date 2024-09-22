c1287cd7bba12738ac4e3e1455111bc3a87926c7
Searching-Sprinkle <l13012032112@gmail.com>
2024-09-22 11:24:00 +0800

    9.22

---
+++ hello_vue3/备份/39$subscribe的使用/src/components/Count.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="count">
+        <h2>
+            当前求和为{{ countStore.sum }}
+        </h2>
+        <h3>
+            地址：{{ countStore.school }},姓名：{{ countStore.name }}
+        </h3>
+        <select v-model.number
+        ="n">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <button @click="add">+</button>
+        <button @click="sub">-</button>
+
+    </div>
+  
+</template>
+
+<script setup lang="ts" name="Count">  
+import {computed,ref} from 'vue'
+import {useCountStore} from '@/store/count'
+import { storeToRefs } from 'pinia';
+
+//countStore里面的数据
+const countStore = useCountStore()
+//伟大，无需多言
+const {sum,school,name} = storeToRefs(countStore)
+
+//数据
+let n = ref(1)//用户选择的数字
+function add(){
+
+    //第一种方法：直接修改
+    // countStore.sum +=n.value
+
+    //第二种方法：补丁式修改
+    // countStore.$patch({
+    //     sum:countStore.sum+n.value,
+    //     school:'尚硅谷',
+    //     name:'Lzz'
+    // })
+
+    //第三种方法：动作修改
+    countStore.increment(n.value)
+}
+function sub(){
+    countStore.decrement(n.value)
+
+}
+
+</script>
+
+<style scoped> 
+.count {
+background-color: skyblue;
+padding: 10px;
+border-radius: 10px;
+box-shadow: 0 0 10px;
+}
+select,button{
+    margin: 0 5px;
+    height: 30px;
+}
+
+</style>
---
+++ hello_vue3/备份/39$subscribe的使用/src/store/count.ts
@@ -0,0 +1,30 @@
+//这就是一个仓库，和count相关的都能存
+import { defineStore } from "pinia";
+export const useCountStore = defineStore('count',{
+    //里面放置的是一个一个的动作函数，用于相应组件中的动作
+    actions:{
+        increment(value:number){
+            this.sum+=value
+            // console.log('increment被调用了',value)
+        },
+        decrement(value:number){
+            this.sum-=value
+            // console.log('decrement被调用了',value)
+        },
+
+    },
+
+    //真正存数据的地方
+    state(){
+        return{
+            sum:6,
+            school:'河北省秦皇岛市海港区',
+            name:'赵小明'
+        }
+    },
+    getters:{
+        bigSum(state){
+            return state.sum*10
+        }
+    }
+});
