50319f06b22620126015179082deb190401c62ee
Searching-Sprinkle <l13012032112@gmail.com>
2024-09-24 09:47:17 +0800

    9.24

--- hello_vue3/src/main.ts
+++ hello_vue3/src/main.ts
@@ -1,14 +1,16 @@
-//引入createApp方法，用于创建应用
-import { createApp } from "vue";
-//引入App根组件
-import App from "./App.vue";
-//引入pinia
-import { createPinia } from "pinia";
+import {createApp} from 'vue'
+import App from './App.vue'
+import {createPinia} from 'pinia'
+import router from './router'
 
-const app = createApp(App);
-//创建pinia
-const pinia = createPinia();
-//安装pinia
-app.use(pinia);
-// app.use(router);
-app.mount("#app");
+// 创建应用
+const app = createApp(App)
+// 创建pinia
+const pinia = createPinia()
+
+// 安装插件
+app.use(pinia)
+// 安装路由器
+app.use(router)
+// 挂载应用
+app.mount('#app')
---
+++ hello_vue3/备份/40store组合式写法/src/APP.vue
@@ -0,0 +1,17 @@
+<template>
+ 
+    <Count/>
+    <br>
+    <LoveTalk/> 
+
+</template>
+
+<script setup lang="ts" name="APP"> 
+import Count from './components/Count.vue';
+import LoveTalk from './components/LoveTalk.vue';
+
+</script>
+
+<style>
+
+</style>
